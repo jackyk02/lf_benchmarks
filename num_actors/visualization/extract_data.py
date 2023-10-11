835cdca0cdc6d82523f78ebd38d44cd34cd6a38a
jackykwok2024 <jackykwok@berkeley.edu>
2023-10-11 13:49:23 -0700

    num_actors benchmarks

---
+++ num_actors/visualization/extract_data.py
@@ -0,0 +1,374 @@
+data = """
+Round: 10
+Total training time: 5.2539 seconds
+Overhead: 0.0169 seconds
+
+Round: 11
+Total training time: 5.7719 seconds
+Overhead: 0.0180 seconds
+
+Round: 12
+Total training time: 6.2897 seconds
+Overhead: 0.0178 seconds
+
+Round: 13
+Total training time: 6.8079 seconds
+Overhead: 0.0182 seconds
+
+Round: 14
+Total training time: 7.3252 seconds
+Overhead: 0.0173 seconds
+
+Round: 15
+Total training time: 7.8432 seconds
+Overhead: 0.0181 seconds
+
+Round: 16
+Total training time: 8.3613 seconds
+Overhead: 0.0180 seconds
+
+Round: 17
+Total training time: 8.8786 seconds
+Overhead: 0.0174 seconds
+
+Round: 18
+Total training time: 9.3966 seconds
+Overhead: 0.0180 seconds
+
+Round: 19
+Total training time: 9.9148 seconds
+Overhead: 0.0182 seconds
+
+Round: 20
+Total training time: 10.4328 seconds
+Overhead: 0.0180 seconds
+
+Round: 21
+Total training time: 10.9504 seconds
+Overhead: 0.0176 seconds
+
+Round: 22
+Total training time: 11.4682 seconds
+Overhead: 0.0178 seconds
+
+Round: 23
+Total training time: 11.9858 seconds
+Overhead: 0.0176 seconds
+
+Round: 24
+Total training time: 12.5036 seconds
+Overhead: 0.0178 seconds
+
+Round: 25
+Total training time: 13.0211 seconds
+Overhead: 0.0175 seconds
+
+Round: 26
+Total training time: 13.5401 seconds
+Overhead: 0.0190 seconds
+
+Round: 27
+Total training time: 14.0589 seconds
+Overhead: 0.0188 seconds
+
+Round: 28
+Total training time: 14.5767 seconds
+Overhead: 0.0178 seconds
+
+Round: 29
+Total training time: 15.0931 seconds
+Overhead: 0.0165 seconds
+
+Round: 30
+Total training time: 15.6111 seconds
+Overhead: 0.0180 seconds
+
+Round: 31
+Total training time: 16.1284 seconds
+Overhead: 0.0173 seconds
+
+Round: 32
+Total training time: 16.6450 seconds
+Overhead: 0.0166 seconds
+
+Round: 33
+Total training time: 17.1616 seconds
+Overhead: 0.0166 seconds
+
+Round: 34
+Total training time: 17.6797 seconds
+Overhead: 0.0181 seconds
+
+Round: 35
+Total training time: 18.1964 seconds
+Overhead: 0.0168 seconds
+
+Round: 36
+Total training time: 18.7138 seconds
+Overhead: 0.0174 seconds
+
+Round: 37
+Total training time: 19.2321 seconds
+Overhead: 0.0183 seconds
+
+Round: 38
+Total training time: 19.7497 seconds
+Overhead: 0.0176 seconds
+
+Round: 39
+Total training time: 20.2669 seconds
+Overhead: 0.0172 seconds
+
+Round: 40
+Total training time: 20.7838 seconds
+Overhead: 0.0169 seconds
+
+Round: 41
+Total training time: 21.3003 seconds
+Overhead: 0.0165 seconds
+
+Round: 42
+Total training time: 21.8175 seconds
+Overhead: 0.0172 seconds
+
+Round: 43
+Total training time: 22.3348 seconds
+Overhead: 0.0172 seconds
+
+Round: 44
+Total training time: 22.8520 seconds
+Overhead: 0.0172 seconds
+
+Round: 45
+Total training time: 23.3712 seconds
+Overhead: 0.0192 seconds
+
+Round: 46
+Total training time: 23.8888 seconds
+Overhead: 0.0177 seconds
+
+Round: 47
+Total training time: 24.4054 seconds
+Overhead: 0.0165 seconds
+
+Round: 48
+Total training time: 24.9231 seconds
+Overhead: 0.0178 seconds
+
+Round: 49
+Total training time: 25.4402 seconds
+Overhead: 0.0171 seconds
+
+Round: 50
+Total training time: 25.9566 seconds
+Overhead: 0.0163 seconds
+
+Round: 51
+Total training time: 26.4732 seconds
+Overhead: 0.0166 seconds
+
+Round: 52
+Total training time: 26.9912 seconds
+Overhead: 0.0180 seconds
+
+Round: 53
+Total training time: 27.5083 seconds
+Overhead: 0.0171 seconds
+
+Round: 54
+Total training time: 28.0251 seconds
+Overhead: 0.0168 seconds
+
+Round: 55
+Total training time: 28.5426 seconds
+Overhead: 0.0174 seconds
+
+Round: 56
+Total training time: 29.0599 seconds
+Overhead: 0.0174 seconds
+
+Round: 57
+Total training time: 29.5787 seconds
+Overhead: 0.0188 seconds
+
+Round: 58
+Total training time: 30.0963 seconds
+Overhead: 0.0176 seconds
+
+Round: 59
+Total training time: 30.6128 seconds
+Overhead: 0.0165 seconds
+
+Round: 60
+Total training time: 31.1311 seconds
+Overhead: 0.0183 seconds
+
+Round: 61
+Total training time: 31.6481 seconds
+Overhead: 0.0171 seconds
+
+Round: 62
+Total training time: 32.1657 seconds
+Overhead: 0.0176 seconds
+
+Round: 63
+Total training time: 32.6835 seconds
+Overhead: 0.0178 seconds
+
+Round: 64
+Total training time: 33.2004 seconds
+Overhead: 0.0169 seconds
+
+Round: 65
+Total training time: 33.7165 seconds
+Overhead: 0.0161 seconds
+
+Round: 66
+Total training time: 34.2329 seconds
+Overhead: 0.0163 seconds
+
+Round: 67
+Total training time: 34.7496 seconds
+Overhead: 0.0167 seconds
+
+Round: 68
+Total training time: 35.2676 seconds
+Overhead: 0.0180 seconds
+
+Round: 69
+Total training time: 35.7845 seconds
+Overhead: 0.0169 seconds
+
+Round: 70
+Total training time: 36.3009 seconds
+Overhead: 0.0165 seconds
+
+Round: 71
+Total training time: 36.8172 seconds
+Overhead: 0.0162 seconds
+
+Round: 72
+Total training time: 37.3360 seconds
+Overhead: 0.0189 seconds
+
+Round: 73
+Total training time: 37.8532 seconds
+Overhead: 0.0171 seconds
+
+Round: 74
+Total training time: 38.3708 seconds
+Overhead: 0.0176 seconds
+
+Round: 75
+Total training time: 38.8886 seconds
+Overhead: 0.0179 seconds
+
+Round: 76
+Total training time: 39.4056 seconds
+Overhead: 0.0170 seconds
+
+Round: 77
+Total training time: 39.9234 seconds
+Overhead: 0.0178 seconds
+
+Round: 78
+Total training time: 40.4404 seconds
+Overhead: 0.0171 seconds
+
+Round: 79
+Total training time: 40.9575 seconds
+Overhead: 0.0171 seconds
+
+Round: 80
+Total training time: 41.4752 seconds
+Overhead: 0.0177 seconds
+
+Round: 81
+Total training time: 41.9924 seconds
+Overhead: 0.0172 seconds
+
+Round: 82
+Total training time: 42.5095 seconds
+Overhead: 0.0171 seconds
+
+Round: 83
+Total training time: 43.0272 seconds
+Overhead: 0.0177 seconds
+
+Round: 84
+Total training time: 43.5441 seconds
+Overhead: 0.0169 seconds
+
+Round: 85
+Total training time: 44.0604 seconds
+Overhead: 0.0164 seconds
+
+Round: 86
+Total training time: 44.5765 seconds
+Overhead: 0.0161 seconds
+
+Round: 87
+Total training time: 45.0937 seconds
+Overhead: 0.0172 seconds
+
+Round: 88
+Total training time: 45.6121 seconds
+Overhead: 0.0183 seconds
+
+Round: 89
+Total training time: 46.1289 seconds
+Overhead: 0.0169 seconds
+
+Round: 90
+Total training time: 46.6444 seconds
+Overhead: 0.0155 seconds
+
+Round: 91
+Total training time: 47.1613 seconds
+Overhead: 0.0169 seconds
+
+Round: 92
+Total training time: 47.6774 seconds
+Overhead: 0.0161 seconds
+
+Round: 93
+Total training time: 48.1956 seconds
+Overhead: 0.0181 seconds
+
+Round: 94
+Total training time: 48.7128 seconds
+Overhead: 0.0173 seconds
+
+Round: 95
+Total training time: 49.2289 seconds
+Overhead: 0.0161 seconds
+
+Round: 96
+Total training time: 49.7466 seconds
+Overhead: 0.0177 seconds
+
+Round: 97
+Total training time: 50.2643 seconds
+Overhead: 0.0176 seconds
+
+Round: 98
+Total training time: 50.7827 seconds
+Overhead: 0.0184 seconds
+
+Round: 99
+Total training time: 51.3003 seconds
+Overhead: 0.0176 seconds
+
+Round: 100
+Total training time: 51.8165 seconds
+Overhead: 0.0162 seconds
+"""
+
+# Extracting overhead values using list comprehension
+overhead_values = [float(line.split()[-2])
+                   for line in data.splitlines() if "Overhead" in line]
+
+# Calculating average overhead
+average_overhead = sum(overhead_values) / len(overhead_values)
+
+print(f"Average Overhead: {average_overhead:.4f} seconds")
--- num_actors/visualization/object_visualization.py
+++ num_actors/visualization/object_visualization.py
@@ -1,32 +1,33 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Object sizes
-object_sizes = [10, 100, 300, 500]
+# Number of Actors
+actors = [2, 4, 8, 16]
 
 # Overhead values for Ray and LF
-ray_overhead = [0.0174, 0.1257, 0.3063, 0.5445]
-ray_overhead = [i * 1000 for i in ray_overhead]
-lf_overhead = [0.0043, 0.0961, 0.2274, 0.4007]
+lf_overhead = [0.0029, 0.0030, 0.0034, 0.0043]
 lf_overhead = [i * 1000 for i in lf_overhead]
+
+ray_overhead = [0.0085, 0.0101, 0.0121, 0.0174]
+ray_overhead = [i * 1000 for i in ray_overhead]
 
 
 # Set the style for the plot
 sns.set_style("whitegrid")
 
 # Plot Ray overhead values
-plt.plot(object_sizes, ray_overhead, marker='D',
+plt.plot(actors, ray_overhead, marker='D',
          label='Ray', linestyle='-', color='blue')
 
 # Plot LF overhead values
-plt.plot(object_sizes, lf_overhead, marker='o',
+plt.plot(actors, lf_overhead, marker='o',
          label='LF', linestyle='-', color='red')
 
 # Labeling the axes and giving a title
-plt.xlabel('Object Size (MB)')
+plt.xlabel('Number of Actors')
 ylabel = plt.ylabel('Overhead (millisecond)')
 ylabel.set_rotation(90)  # explicitly set rotation to 0 for vertical alignment
-plt.title('Mean Overhead of Broadcast and Gather on 16 actors \n for Ray v2.5.1 and LF')
+plt.title('Mean Overhead of Broadcast and Gather 10MB Object \n with Different Number of Actors')
 
 # Displaying the legend
 plt.legend()
